dd63f5069e8853b6430a7bab2fe5805825273102
Willem April <willemapril33@gmail.com>
2025-07-17 20:58:24 +0200

    Add screen overlay for sidebar interaction
    Introduces a screen overlay element to the main layout, styled to appear when the sidebar is visible. Overlay can be clicked to hide the sidebar, improving user experience and accessibility.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const menuButton = document.querySelectorAll(".menu-button");
+const screenOverlay = document.querySelector(".screen-overlay");
 
 menuButton.forEach(button => {
     button.addEventListener("click", () => {
     document.body.classList.toggle("sidebar-hidden")
 })
+});
+
+screenOverlay.addEventListener("click", () => {
+    document.body.classList.toggle("sidebar-hidden")
 })
