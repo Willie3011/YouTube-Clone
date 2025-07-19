7ad2bbbf5de05bbf3d693571ffe27d7be28a8155
Willem April <willemapril33@gmail.com>
2025-07-19 20:48:02 +0200

    Add dark mode toggle and update styles
    Implemented a dark mode toggle using localStorage and updated theme button logic in script.js. Added dark mode CSS variables and adjusted styles for better theme support in style.css.

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const menuButton = document.querySelectorAll(".menu-button");
 const screenOverlay = document.querySelector(".screen-overlay");
+const themeButton = document.querySelector(".theme-button i");
 
+if(localStorage.getItem("darkMode") === "enabled"){
+    document.body.classList.add("dark-mode");
+    themeButton.classList.replace("fa-moon", "fa-sun")
+} else {
+    themeButton.classList.replace("fa-sun", "fa-moon");
+}
+
+//Toggle dark mode when theme button is clicked
+themeButton.addEventListener("click", () => {
+   const isDarkMode = document.body.classList.toggle("dark-mode")
+   localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled")
+   themeButton.classList.toggle("fa-sun", isDarkMode)
+   themeButton.classList.toggle("fa-moon", !isDarkMode)
+})
+
+//Toggle sidebar visibility when menu buttons are clicked
 menuButton.forEach(button => {
     button.addEventListener("click", () => {
     document.body.classList.toggle("sidebar-hidden")
 })
 });
 
+//Toggle sidebar visibility when screen overlay is clicked
 screenOverlay.addEventListener("click", () => {
     document.body.classList.toggle("sidebar-hidden")
 })
 
+//show sidebar on large screens by default
 if(window.innerWidth >= 768) {
     document.body.classList.remove("sidebar-hidden")
 }
